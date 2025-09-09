64d082951bf490864537cb81a2b05245f6793458
JodieFlacko <96775468+JodieFlacko@users.noreply.github.com>
2025-09-09 18:18:06 +0200

    feat: implement logic that update game status

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 const Game = (function(){  
   const Gameboard = (function (){
     const board = [
-    ]
-    const winningSeries = [
-      [0, 1, 2],  // top row
-      [3, 4, 5],  // middle row
-      [6, 7, 8],  // bottom row
-      [0, 3, 6],  // left column
-      [1, 4, 7],  // middle column
-      [2, 5, 8],  // right column
-      [0, 4, 8],  // diagonal (\)
-      [2, 4, 6]   // diagonal (/)
     ];
 
     (function populateBoard(){
@@ -25,9 +15,9 @@ const Game = (function(){
       }
     }
 
-    function checkBoard(){
-      const updatedBoard = board.filter(cell => cell.getValue() != "")
-      if (updatedBoard.length === 9) return true
+    function getBoard(){
+      const boardValues = board.map(cell => cell.getValue())
+      return boardValues
     }
 
     function markCell(boardIndex, mark){
@@ -46,7 +36,7 @@ const Game = (function(){
     }
 
 
-    return {showBoard, checkBoard, markCell}
+    return {showBoard, getBoard, markCell}
   }())
 
   const Players = (function(){
@@ -79,13 +69,14 @@ const Game = (function(){
     const playerOne = Players.one
     const playerTwo = Players.two
     let activePlayer = playerOne
-    let gameOver = false
+    const mark = activePlayer.getMark()
+    let gameStatus = "ongoing"
 
     function displayPlayersData(){
       console.log(`First Player \n\tName: ${playerOne.getName()}\n\tScore: ${playerOne.getScore()}\n\tMark: ${playerOne.getMark()}\nSecond Player\n\tName: ${playerTwo.getName()}\n\tScore: ${playerTwo.getScore()}\n\tMark: ${playerTwo.getMark()}\n`)
     }
 
-    function startRound(message){
+    function playTurn(message){
       console.log(message);
       // function that allow the user to choose the cell to mark
       (function promptCellToMark(){
@@ -101,32 +92,80 @@ const Game = (function(){
         columnIndex -= 1
 
         const boardIndex = (rowIndex * 3) + columnIndex
-        const mark = activePlayer.getMark()
 
         Gameboard.markCell(boardIndex, mark)
 
+        //test
+        Gameboard.markCell(1, mark)
+        Gameboard.markCell(2, mark)
+
       })();
 
-      (function switchRound(){
+      (function updateGameStatus(){
+        const winningSeries = [
+          [0, 1, 2],  // top row
+          [3, 4, 5],  // middle row
+          [6, 7, 8],  // bottom row
+          [0, 3, 6],  // left column
+          [1, 4, 7],  // middle column
+          [2, 5, 8],  // right column
+          [0, 4, 8],  // diagonal (\)
+          [2, 4, 6]   // diagonal (/)
+        ];
+
+        const board = Gameboard.getBoard();
+        (function checkWin(){
+          winningSeries.forEach(array => {
+            let count = 0
+            array.forEach(index =>{
+              if(board[index] == mark){
+                count++;
+                if (count === 3) gameStatus = "win"
+                return
+              }
+            })
+          });
+        })();
+
+        (function checkTie(){
+          const markedBoard = board.filter(cell => cell != "")
+          if (markedBoard.length === 9) {
+            gameStatus = "tie"
+            return
+          }
+        })();
+      })();
+
+      (function switchTurn(){
         activePlayer = activePlayer === playerOne ? playerTwo : playerOne
       })();
 
       function checkPositionInputValidity(inputX, inputY){
         if(inputX > 3 || inputX < 1 && inputY > 3 || inputY < 1) {
-          startRound('Invalid Input: enter a value greater then 0 and smaller then 4')
+          playTurn('Invalid Input: enter a value greater then 0 and smaller then 4')
           return
         }
       }
       Gameboard.showBoard();
-      gameOver = Gameboard.checkBoard();
     }
     
-    // while(!gameOver){
-    //   startRound()
-    // }
+    function checkGameStatus(){
+      switch (gameStatus){
+        case "ongoing":
+          playTurn()
+          return;
+        case "win":
+          console.log("We have a winner!")
+          return;
+        case "tie":
+          console.log("we have a Tie!")
+          return;
+      }
+    };
 
-    startRound()
 
+    playTurn()
+    checkGameStatus()
     return {displayPlayersData}
   }())
 
