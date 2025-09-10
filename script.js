4369ace5181d62e5cea84cf97b2a1806896ca161
JodieFlacko <96775468+JodieFlacko@users.noreply.github.com>
2025-09-10 12:57:50 +0200

    feat: end match when a player wins 5 times

--- script.js
+++ script.js
@@ -72,117 +72,143 @@ const Game = (function(){
   }());
 
   const consoleController = (function(){
-    const playerOne = Players.one;
-    const playerTwo = Players.two;
-    let activePlayer = playerOne;
-    const activePlayerMark = activePlayer.getMark();
-    let gameStatus = "ongoing";
-    let ties = 0
-    function startRound(){
-      while(gameStatus === 'ongoing'){
-        (function playTurn(message){
-          console.log(message);
-          // function that allow the user to choose the cell to activePlayerMark
-          (function promptCellToMark(){
-            // const rowIndex = prompt("Enter a valid row: ")
-            // const columnIndex = promptF("Enter a valid column: ")
-            // test
-            let rowIndex = '1';
-            let columnIndex = '1';
-            // check for input validity
-            (function checkPositionInputValidity(inputX, inputY){
-              if(inputX > 3 || inputX < 1 && inputY > 3 || inputY < 1) {
-                playTurn('Invalid Input: enter a value greater then 0 and smaller then 4')
-                return
-              }
+    function startMatch(){
+      const playerOne = Players.one;
+      const playerTwo = Players.two;
+      let activePlayer = playerOne;
+      const activePlayerMark = activePlayer.getMark();
+      let roundStatus = "ongoing";
+      let gameStatus = "ongoing";
+      let ties = 0;
+      let playedRounds = 0;
+      function startRound(){
+        while(roundStatus === 'ongoing'){
+          (function playTurn(message){
+            console.log(message);
+            // function that allow the user to choose the cell to activePlayerMark
+            (function promptCellToMark(){
+              // const rowIndex = prompt("Enter a valid row: ")
+              // const columnIndex = promptF("Enter a valid column: ")
+              // test
+              let rowIndex = '1';
+              let columnIndex = '1';
+              // check for input validity
+              (function checkPositionInputValidity(inputX, inputY){
+                if(inputX > 3 || inputX < 1 && inputY > 3 || inputY < 1) {
+                  playTurn('Invalid Input: enter a value greater then 0 and smaller then 4')
+                  return
+                }
+              })();
+
+              // subtract one from indexes to access the right position
+              rowIndex -= 1;
+              columnIndex -= 1;
+              const boardIndex = (rowIndex * 3) + columnIndex;
+
+              Gameboard.markCell(boardIndex, activePlayerMark);
+
+              //test
+              Gameboard.markCell(1, activePlayerMark);
+              Gameboard.markCell(2, activePlayerMark);
+
             })();
 
-            // subtract one from indexes to access the right position
-            rowIndex -= 1;
-            columnIndex -= 1;
-            const boardIndex = (rowIndex * 3) + columnIndex;
+            (function updateRoundStatus(){
+              const winningSeries = [
+                [0, 1, 2],  // top row
+                [3, 4, 5],  // middle row
+                [6, 7, 8],  // bottom row
+                [0, 3, 6],  // left column
+                [1, 4, 7],  // middle column
+                [2, 5, 8],  // right column
+                [0, 4, 8],  // diagonal (\)
+                [2, 4, 6]   // diagonal (/)
+              ];
 
-            Gameboard.markCell(boardIndex, activePlayerMark);
-
-            //test
-            Gameboard.markCell(1, activePlayerMark);
-            Gameboard.markCell(2, activePlayerMark);
-
-          })();
-
-          (function updateGameStatus(){
-            const winningSeries = [
-              [0, 1, 2],  // top row
-              [3, 4, 5],  // middle row
-              [6, 7, 8],  // bottom row
-              [0, 3, 6],  // left column
-              [1, 4, 7],  // middle column
-              [2, 5, 8],  // right column
-              [0, 4, 8],  // diagonal (\)
-              [2, 4, 6]   // diagonal (/)
-            ];
-
-            const board = Gameboard.getBoard();
-            (function checkWin(){
-              winningSeries.forEach(array => {
-                let count = 0
-                array.forEach(index =>{
-                  if(board[index] == activePlayerMark){
-                    count++;
-                    if (count === 3) {
-                      gameStatus = "win"
-                      activePlayer.increaseScore();
-                      return;
+              const board = Gameboard.getBoard();
+              (function checkWin(){
+                winningSeries.forEach(array => {
+                  let count = 0
+                  array.forEach(index =>{
+                    if(board[index] == activePlayerMark){
+                      count++;
+                      if (count === 3) {
+                        roundStatus = "win"
+                        activePlayer.increaseScore();
+                        return;
+                      }
                     }
-                  }
-                })
-              });
-            })();
+                  })
+                });
+              })();
 
-            (function checkTie(){
-              const markedBoard = board.filter(cell => cell != "")
-              if (markedBoard.length === 9) {
-                gameStatus = "tie";
-                ties++;
-                return;
-              }
-            })();
-          })();  
+              (function checkTie(){
+                const markedBoard = board.filter(cell => cell != "")
+                if (markedBoard.length === 9) {
+                  roundStatus = "tie";
+                  ties++;
+                  return;
+                }
+              })();
+            })();  
 
-          function logUpdate(status){
-            if(status === "win"){
-              console.log(`We have a winner! ${activePlayer.getName()} wins the round!`);
-            }
-            else{
-              console.log("We have a Tie!");
-            }
-          };
-
-          switchTurn();
-
-          Gameboard.showBoard();
-          logUpdate(gameStatus);
-          displayScore();
+            Gameboard.showBoard();
+            logUpdate(roundStatus);
+            displayScore();
+          })();
+        };
+        // while loop stops, end round
+        (function endRound(){
+          Gameboard.clearBoard();
+          roundStatus = "ongoing";
         })();
+        
+        // update played rounds
+        playedRounds++;
+
+        //condition to end match
+        (function updateGameStatus(){
+          const activePlayerScore = activePlayer.getScore();
+          if(activePlayerScore === 5) {
+            gameStatus = "end";
+            logUpdate(gameStatus);
+          }
+        })();
+
+        if(gameStatus === "end") return;
+        //start another round if condition is not fulfilled
+        (function switchTurn(){
+              activePlayer = activePlayer === playerOne ? playerTwo : playerOne
+        })();
+        startRound();
+      }
+      
+      //helper functions
+      function displayScore(){
+        console.log(`First Player \n\tName: ${playerOne.getName()}\n\tScore: ${playerOne.getScore()}\n\tMark: ${playerOne.getMark()}\nSecond Player\n\tName: ${playerTwo.getName()}\n\tScore: ${playerTwo.getScore()}\n\tMark: ${playerTwo.getMark()}\n\t\nTies: ${ties}`)
       };
 
-      // if we exit the while loop, the round has ended
-      Gameboard.clearBoard();
-
-      function switchTurn(){
-      activePlayer = activePlayer === playerOne ? playerTwo : playerOne
+      function logUpdate(status){
+        const name = activePlayer.getName();
+        if(status === "win"){
+          console.log(`We have a winner! ${name} wins the round!`);
+        }
+        else if(status === "tie"){
+          console.log("We have a Tie!");
+        }
+        else if(status === "end"){
+          console.log(`${name} wins the game!`);
+        }
       };
-    }
-    
-    function displayScore(){
-      console.log(`First Player \n\tName: ${playerOne.getName()}\n\tScore: ${playerOne.getScore()}\n\tMark: ${playerOne.getMark()}\nSecond Player\n\tName: ${playerTwo.getName()}\n\tScore: ${playerTwo.getScore()}\n\tMark: ${playerTwo.getMark()}\n\t\nTies: ${ties}`)
+
+      startRound();
     };
 
-    startRound();
-
-    return {displayScore};
+  //"consoleController" module ending
+  return {startMatch}
   }());
 
+
+  consoleController.startMatch();
   // "Game" module ending
-  return {displayData: consoleController.displayScore};
 })();
